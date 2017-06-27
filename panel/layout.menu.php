64a66072c21bb856a57e3e4267cf76ec6cd88974
Diego Capape <diego.capape@gmail.com>
2017-06-27 16:09:15 +0200

    menu

--- panel/layout.menu.php
+++ panel/layout.menu.php
@@ -1,9 +1,9 @@
 <div id="menu">
   <ul>
-    <li><a href='?module=home' class='<?php if ($module=='home') echo 'selected';?>>'>Portada</a></li>
-    <li><a href='?module=sip_buddies' class='<?php if ($module=='sip_buddies') echo 'active';?>>'>Extensiones SIP</a></li>
-    <li><a href='?module=sip_buddies' class='<?php if ($module=='queues') echo 'active';?>>'>Colas</a></li>
-    <li><a href='?module=sip_buddies' class='<?php if ($module=='meetme') echo 'active';?>>'>Salas de conferencia</a></li>
+    <li><a href='?module=home' class='<?php if ($module=='home') echo 'active';?>'>Portada</a></li>
+    <li><a href='?module=sip_buddies' class='<?php if ($module=='sip_buddies') echo 'active';?>'>Extensiones SIP</a></li>
+    <li><a href='?module=sip_buddies' class='<?php if ($module=='queues') echo 'active';?>'>Colas</a></li>
+    <li><a href='?module=sip_buddies' class='<?php if ($module=='meetme') echo 'active';?>'>Salas de conferencia</a></li>
   </ul>
 </div>
 
