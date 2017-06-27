d4fa18391b0237c72be70225d32ada374e3ba91b
Diego Capape <diego.capape@gmail.com>
2017-06-27 12:41:39 +0200

    menu

--- panel/layout.menu.php
+++ panel/layout.menu.php
@@ -1,4 +1,18 @@
 <div id="menu">
-  <a href='?module=home' class='<?php if ($module=='home') ? echo 'selected';?>>'>Portada</a>
-  <a href='?module=sip_buddies' class='<?php if ($module=='sip_buddies') ? echo 'selected';?>>'>Extensiones SIP</a>
+  <a href='?module=home' class='<?php if ($module=='home') echo 'selected';?>>'>Portada</a>
+  <a href='?module=sip_buddies' class='<?php if ($module=='sip_buddies') echo 'selected';?>>'>Extensiones SIP</a>
+  <a href='?module=sip_buddies' class='<?php if ($module=='queues') echo 'selected';?>>'>Colas</a>
+  <a href='?module=sip_buddies' class='<?php if ($module=='meetme') echo 'selected';?>>'>Salas de conferencia</a>
+</div>
+
+<div id="submenu">
+<?php if ($module == 'sip_buddies') {?>
+  <a href='?module=sip_buddies&view=list'>Lista</a>
+  <a href='?module=sip_buddies&view=add'>Anadir</a>
+  <a href='?module=sip_buddies&view=edit'>Editar</a>
+<?php } elseif ($module == 'voicemail') {?>
+
+<?php } else {?>
+
+<?php }?>
 </div>
