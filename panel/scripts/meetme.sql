17d7bccd4b6175f00c58373bc3a02dafd3479b75
Diego Capape <diego.capape@gmail.com>
2017-06-27 18:21:40 +0200

    meetme

--- panel/scripts/meetme.sql
+++ panel/scripts/meetme.sql
@@ -1,13 +1,68 @@
-CREATE TABLE `meetme` (
-  `confno` varchar(80) NOT NULL default '0',
-  `username` varchar(64) NOT NULL default '',
-  `domain` varchar(128) NOT NULL default '',
-  `pin` varchar(20) default NULL,
-  `adminpin` varchar(20) default NULL,
-  `members` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`confno`)
-  ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 27-06-2017 a las 18:19:55
+-- Versión del servidor: 5.5.55-0+deb8u1
+-- Versión de PHP: 5.6.30-0+deb8u1
 
-  INSERT INTO `meetme` (`confno`,`username`,`domain`,`pin`,`adminpin`,`members`)
-   VALUES
-   ('3001','','','5555','4444',0);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `asterisk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `meetme`
+--
+
+CREATE TABLE IF NOT EXISTS `meetme` (
+`id` int(5) NOT NULL,
+  `confno` varchar(80) NOT NULL DEFAULT '0',
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `domain` varchar(128) NOT NULL DEFAULT '',
+  `pin` varchar(20) DEFAULT NULL,
+  `adminpin` varchar(20) DEFAULT NULL,
+  `members` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `meetme`
+--
+
+INSERT INTO `meetme` (`id`, `confno`, `username`, `domain`, `pin`, `adminpin`, `members`) VALUES
+(1, '3001', '', '', '5555', '4444', 0),
+(2, '3002', '', '', '1111', '1112', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `meetme`
+--
+ALTER TABLE `meetme`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `meetme`
+--
+ALTER TABLE `meetme`
+MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
