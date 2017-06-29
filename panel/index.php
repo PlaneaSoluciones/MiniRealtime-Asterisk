1828f58b4644dfe75a2822bffd52f3ec64aa839e
Diego Capape <diego.capape@gmail.com>
2017-06-29 21:04:01 +0200

    click2call 1

--- panel/index.php
+++ panel/index.php
@@ -12,6 +12,8 @@ require 'class.db.php';
     $module = (isset($_GET['module']) ? $_GET['module'] : null);
     $view   = (isset($_GET['view'])   ? $_GET['view'] : null);
     $id     = (isset($_GET['id'])     ? $_GET['id'] : null);
+    $ext1   = (isset($_GET['ext1'])   ? $_GET['ext1'] : null);
+    $ext2   = (isset($_GET['ext2'])   ? $_GET['ext2'] : null);
 
     //POST ROUTES
     $type   = (isset($_POST['type'])  ? $_POST['type'] : null);
