d92b0b1bba84bee12d67dbe1882df48027ee8ba0
nfortea <nfortea@planeasoluciones.com>
2017-06-30 16:13:05 +0200

    Hardcoded insertion on table queue_members.

--- panel/controller.queues.php
+++ panel/controller.queues.php
@@ -299,7 +299,7 @@ function addmembers($nameq, $postType) {
     $addname   = (isset($_POST['addname'])  ? $_POST['addname'] : null);
 
     if ($addname != null){ // ADD NEW ENTRY
-       $query="insert into queue_members (membername, queue_name) values ('$addname', '$nameq')";
+       $query="insert into queue_members (membername, queue_name) values ('local/$addname@internas', '$nameq')";
        db::getInstance()->query($query);
     }
   }
